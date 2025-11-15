70c62e42c913a3460ea7d41d73ad4df2bc5c91b5
Anyasan <siddhirai326@gmail.com>
2025-11-15 21:00:51 +0530

    started js basic

---
+++ script.js
@@ -0,0 +1,20 @@
+// console.log("hello");
+// window.alert("good job");
+
+// document.getElementById("hi").textContent = 'hello';
+// document.getElementById("pa").textContent = "this is a get element by id ";
+
+// let x=20;
+// let y=30;
+// console.log(x+y);
+
+// let age;
+// age=80;
+// if (age<=18){
+//     console.log(`ur underage as ur ${age}`);
+// }
+// else{
+//     console.log(`welcome as ur ${age} which is more that 18`);
+// }
+
+
