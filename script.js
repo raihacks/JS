c6b68eabfc859d96746a0c2ee25f397f330f59fe
Anyasan <siddhirai326@gmail.com>
2025-11-19 10:56:52 +0530

    randomNum

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 //random number generator
+// const min=50;
+// const max=100;
+// let randomNum = Math.floor (Math.random()*(max-min)) + min;
+// console.log(randomNum);
 
-let randomNum = Math.random();
-console.log(randomNum);
+// const bnt=document.getElementById("bnt");
+// const lable1=document.getElementById("lable1");
+// const lable2=document.getElementById("lable2");
+// const lable3=document.getElementById("lable3");
+// const min = 1;
+// const max = 6;
+// let randomNum1;
+// let randomNum2;
+// let randomNum3;
+// bnt.onclick = function(){
+//     randomNum1 = Math.floor(Math.random()*max) + min;
+//     randomNum2 = Math.floor(Math.random()*max) + min;
+//     randomNum3 = Math.floor(Math.random()*max) + min;
+//     lable1.textContent=randomNum1;
+//     lable2.textContent=randomNum2;
+//     lable3.textContent=randomNum3;
+// }
+
