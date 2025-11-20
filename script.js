0e1a0007f93592f191a0f4b1c08c0eafd2515e46
raihacks <siddhirai326@gmail.com>
2025-11-20 20:09:14 +0530

    method chaining

--- script.js
+++ script.js
@@ -119,4 +119,13 @@
 // console.log(firstName);
 // console.log(lastName);
 
-
+// let userName = window.prompt("Enter your username: ");
+// userName = userName.trim();
+// let letter = userName.charAt(0);
+// letter = letter.toUpperCase();
+// let extraChars = userName.slice(1);
+// extraChars = extraChars.toLowerCase();
+// userName = letter+extraChars;
+// console.log(userName);
+// userName = userName.trim().charAt().toUpperCase() +  userName.trim().slice(1).toLowerCase();
+// console.log(userName);
