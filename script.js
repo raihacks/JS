689ce592a11de4f9ad78057595afc071bd45e82c
raihacks <siddhirai326@gmail.com>
2025-11-21 09:29:16 +0530

    while

---
+++ script.js
@@ -0,0 +1,13 @@
+// const temp = -20;
+// if(temp > 0 && temp <= 30){
+//     console.log("The weather is GOOD");
+// }
+// else{
+//     console.log("the wether is BAD")
+// }
+
+let username =" ";
+while(username === " "){
+    username=window.prompt("entername");
+}
+console.log(`hello ${username}`);
