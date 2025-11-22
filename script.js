caa740e18cebbb58bc99a61d50632ad8c28e4273
raihacks <siddhirai326@gmail.com>
2025-11-22 17:19:16 +0530

    done convert

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function convTemp(){
-    
-}
+// const inputBox= document.getElementById("inputBox");
+// const c2f=document.getElementById("c2f");
+// const f2c=document.getElementById("f2c");
+// const result=document.getElementById("result");
+// let temp;
+// function convTemp(){
+//     if (c2f.checked){
+//         temp = Number(inputBox.value);
+//         temp = temp*(9/5)+32;
+//         result.textContent = temp.toFixed(1) + "F";
+//     }
+//     else if(f2c.checked){
+//         temp = Number(inputBox.value);
+//         temp = (temp-32)*(5/9);
+//         result.textContent = temp.toFixed(1) + "C";
+//     }
+//     else{
+//         result.textContent = "Select a unit";
+//     }
+// }
